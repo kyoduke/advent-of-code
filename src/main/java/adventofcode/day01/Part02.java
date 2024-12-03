e7efbd16705322123bdfef429fa497461e155b60
Caio Duque <caioduque.dev@gmail.com>
2024-12-03 16:25:16 -0300

    added day - one part two solution

--- src/main/java/adventofcode/day01/Part02.java
+++ src/main/java/adventofcode/day01/Part02.java
@@ -8,13 +8,30 @@ import java.util.List;
 public class Part02 {
     public void result() {
         List<String> inputLines = InputReader.readInputByLine("day01/input.txt", this.getClass());
-        var firstCol = new ArrayList<>();
-        var secondCol = new ArrayList<>();
+        var firstCol = new ArrayList<Integer>();
+        var secondCol = new ArrayList<Integer>();
         for (String line : inputLines ) {
             String[] splittedLine = line.split(" {3}");
-            firstCol.add(splittedLine[0]);
-            secondCol.add(splittedLine[1]);
+            firstCol.add(Integer.parseInt(splittedLine[0]));
+            secondCol.add(Integer.parseInt(splittedLine[1]));
         }
+        firstCol.sort(null);
+        secondCol.sort(null);
+
+        int total = 0;
+        for (int firstColNumber : firstCol) {
+            int count = 0;
+            for (int secondColNumber : secondCol) {
+                if (firstColNumber == secondColNumber) {
+                    count++;
+                }
+            }
+            total += (firstColNumber * count);
+        }
+        System.out.println(total);
+
+
+
 
     }
     public static void main(String[] args) {
