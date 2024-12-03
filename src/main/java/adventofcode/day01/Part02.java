c402f8b5d670dbd2a6d87df2d4651b5813fb07d9
Caio Duque <caioduque.dev@gmail.com>
2024-12-02 21:28:54 -0300

    wip: part 2 solution

--- src/main/java/adventofcode/day01/Part02.java
+++ src/main/java/adventofcode/day01/Part02.java
@@ -1,4 +1,24 @@
 package adventofcode.day01;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Part02 {
+    public static void main(String[] args) {
+        try {
+            File file = new File("src/main/resources/day01.txt");
+            Scanner scanner = new Scanner(file);
+            while(scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                String[] splittedLine = line.split("   ");
+                int firstValue, secondValue;
+                firstValue = Integer.parseInt(splittedLine[0]);
+                secondValue = Integer.parseInt(splittedLine[1]);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+            e.printStackTrace();
+        }
+    }
 }
